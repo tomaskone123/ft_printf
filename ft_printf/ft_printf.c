2e7780c19d24c7a9b2711b46467d8679027ee7aa
Tomas Konecny <tkonecny@c1r1s5.42prague.com>
2024-06-17 15:43:36 +0200

    final version

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tkonecny <tkonecny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 15:59:33 by tkonecny          #+#    #+#             */
-/*   Updated: 2024/06/16 19:19:40 by tkonecny         ###   ########.fr       */
+/*   Updated: 2024/06/17 15:38:48 by tkonecny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,3 +38,70 @@ int	ft_printf(const char *format, ...)
 	va_end(arg);
 	return (v.i);
 }
+
+// int	main(void)
+// {
+// 	char			c = 'h';
+// 	char			*s = "hello";
+// 	int				d = 123;
+// 	int				i = 456;
+// 	unsigned int	u = -123;
+// 	int				x = -1234;
+// 	int				X = -1234;
+
+// 	ft_printf(" aa%caa aa%saa aa%paa aa%daa aa%iaa aa%uaa aa%xaa aa%Xaa aa%%aa\n", c, s, &s, d, i, u, x, X);
+// 	printf(" aa%caa aa%saa aa%paa aa%daa aa%iaa aa%uaa aa%xaa aa%Xaa aa%%aa\n", c, s, &s, d, i, u, x, X);
+// 	ft_printf("%%%%%u\n", u);
+// 	printf("%%%%%u\n", u);
+// 	ft_printf("%%%%%s\n", s);
+// 	printf("%%%%%s\n", s);
+// 	ft_printf("%%%p%%\n", &u);
+// 	printf("%%%p%%\n", &u);
+
+// }
+
+// #include "ft_printf.h"
+// #include "libft/libft.h"
+// #include <stdio.h>
+// // Include your ft_printf header
+
+// int main(void) {
+//     // Test case for %c
+//     printf("ft_printf: %d\n", ft_printf("Character: %c\n", 'A'));
+//     printf("printf: %d\n", printf("Character: %c\n", 'A'));
+
+//     // Test case for %s
+//     printf("ft_printf: %d\n", ft_printf("String: %s\n", "Hello, World!"));
+//     printf("printf: %d\n", printf("String: %s\n", "Hello, World!"));
+
+//     // Test case for %p
+//     int x = 42;
+//     printf("ft_printf: %d\n", ft_printf("Pointer: %p\n", &x));
+//     printf("printf: %d\n", printf("Pointer: %p\n", &x));
+
+//     // Test case for %d
+//     printf("ft_printf: %d\n", ft_printf("Decimal: %d\n", 1234));
+//     printf("printf: %d\n", printf("Decimal: %d\n", 1234));
+
+//     // Test case for %i
+//     printf("ft_printf: %d\n", ft_printf("Integer: %i\n", -1234));
+//     printf("printf: %d\n", printf("Integer: %i\n", -1234));
+
+//     // Test case for %u
+//     printf("ft_printf: %d\n", ft_printf("Unsigned: %u\n", 4294967295U));
+//     printf("printf: %d\n", printf("Unsigned: %u\n", 4294967295U));
+
+//     // Test case for %x
+//     printf("ft_printf: %d\n", ft_printf("Hexadecimal (lowercase): %x\n", 0xabcdef));
+//     printf("printf: %d\n", printf("Hexadecimal (lowercase): %x\n", 0xabcdef));
+
+//     // Test case for %X
+//     printf("ft_printf: %d\n", ft_printf("Hexadecimal (uppercase): %X\n", 0xABCDEF));
+//     printf("printf: %d\n", printf("Hexadecimal (uppercase): %X\n", 0xABCDEF));
+
+//     // Test case for %%
+//     printf("ft_printf: %d\n", ft_printf("Percent sign: %%\n"));
+//     printf("printf: %d\n", printf("Percent sign: %%\n"));
+
+//     return 0;
+// }
